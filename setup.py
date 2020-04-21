1afe19de99924b7d863dd14e1d9cc9aaacb2d475
Derek Fujimoto <fujimoto@phas.ubc.ca>
2020-04-20 21:32:31 -0700

    v1.0.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ ext = Extension("mudpy.mud_friendly",
 
 setuptools.setup(
     name="mud-py",
-    version="1.0.0",
+    version="1.0.1",
     author="Derek Fujimoto",
     author_email="fujimoto@phas.ubc.ca",
     description="MUon Data file reader",

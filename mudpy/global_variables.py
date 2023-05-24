d36a07aa86b5bb109bd46898e5325f540b424d62
dfujimoto <dfujimoto@triumf.ca>
2023-05-24 12:12:21 -0700

    v1.2.12: fixed install issues

--- mudpy/global_variables.py
+++ mudpy/global_variables.py
@@ -1,3 +1,3 @@
-__version__ = '1.2.11'
+__version__ = '1.2.12'
 __author__ = 'Derek Fujimoto'
 __src__ = 'mud_src'
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import numpy
 from os.path import join
@@ -9,12 +9,13 @@ with open(join('mudpy', 'global_variables.py')) as fid:
     exec(fid.read(), variables)
 
 __src__ = variables['__src__']
+__version__ = variables['__version__']
 
 # get needed compile arguments
 compile_flags = ['-O3']
 
 # module extension
-ext = setuptools.Extension("mudpy.mud_friendly",
+ext = Extension("mudpy.mud_friendly",
                 sources=[join("mudpy", "mud_friendly_wrapper.pyx"),
                          join(__src__, "mud.c"),
                          join(__src__, "mud_gen.c"),
@@ -26,8 +27,10 @@ ext = setuptools.Extension("mudpy.mud_friendly",
                 include_dirs = [__src__, numpy.get_include()],
                 extra_compile_args = compile_flags)
 
-setuptools.setup(
+setup(
     name = "mud-py",
+    version = __version__,
+    packages=find_packages(),
     ext_modules = cythonize([ext],
                             include_path = [numpy.get_include()],
                             compiler_directives={"language_level" : "3"})
